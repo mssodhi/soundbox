410054e5e8177cbe731715149b46d4d2c7cbbbaa
Manu SOdhi <mssodhi@ucdavis.edu>
2016-04-14 00:36:06 -0700

    Made the player footer

--- src/main/webapp/resources/scripts/controllers/landing/landing.js
+++ src/main/webapp/resources/scripts/controllers/landing/landing.js
@@ -16,15 +16,6 @@ angular.module('app').controller('LandingCtrl', ['$location', 'CredentialsServic
 
         ctrl.getFavorites();
 
-        //if(ctrl.currentUser.location){
-        //    $http({method: 'GET',
-        //        url: 'http://api.openweathermap.org/data/2.5/weather?q='+ ctrl.currentUser.location +',us&appid=44db6a862fba0b067b1930da0d769e98'
-        //    }).then(function successCallback(response) {
-        //        ctrl.weather = response.data;
-        //        ctrl.temp = (response.data.main.temp - 273.15) * 1.8000 + 32.00 + '';
-        //        ctrl.temp = ctrl.temp.substring(0, 5);
-        //    });
-        //}
     };
 
     ctrl.getFavorites = function(){
