fda0163f4c2288bf725cbefe23c06ca2e6a93cfe
Manu SOdhi <mssodhi@ucdavis.edu>
2016-06-05 01:10:16 -0700

    changed the login form to a modal.

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -43,6 +43,7 @@
 <script src="resources/scripts/controllers/landing/landing.js"></script>
 <script src="resources/scripts/controllers/nav/nav.js"></script>
 <script src="resources/scripts/controllers/login/login.js"></script>
+<script src="resources/scripts/controllers/login/login-modal.js"></script>
 <script src="resources/scripts/controllers/hot-n-new/hot-n-new.js"></script>
 <script src="resources/scripts/controllers/media/music.js"></script>
 <script src="resources/scripts/controllers/media/video.js"></script>
---
+++ src/main/webapp/resources/scripts/controllers/login/login-modal.js
@@ -0,0 +1,69 @@
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+'use strict';
+
+angular.module('app').controller('LoginModalCtrl', function ($uibModalInstance, UserService, $location) {
+
+    var ctrl = this;
+    
+    ctrl.init = function () {
+        ctrl.showLoginForm = true;
+        ctrl.showRegisterForm = false;
+        ctrl.user = {};
+    };
+
+    ctrl.switchForm = function (form) {
+        if(form === 'login'){
+            ctrl.init();
+        }else{
+            ctrl.showRegisterForm = true;
+            ctrl.showLoginForm = false;
+        }
+    };
+    
+    ctrl.login = function(){
+        ctrl.noUserFound = false;
+        UserService.login({email: ctrl.email}, ctrl.password).$promise.then(function (response) {
+            if(response.id){
+                ctrl.goToLanding();
+            }else{
+                ctrl.noUserFound = true;
+            }
+        });
+    };
+    
+    ctrl.register = function(){
+        ctrl.passwordNotSame = false;
+        if(ctrl.password === ctrl.repeatedPassword){
+            var user = {name: ctrl.name, email: ctrl.email, password: ctrl.password};
+            UserService.checkEmailAvailability({email: ctrl.email}).$promise.then(function (response) {
+                if(response.taken !== 'true'){
+                    UserService.addUser(user).$promise.then(function(response){
+                        if(response){
+                            ctrl.goToLanding();
+                        }else{
+                            $location.path('/deny');
+                        }
+                    });
+                }else{
+                    ctrl.emailTaken = true;
+                }
+            });
+        }else{
+            ctrl.passwordNotSame = true;
+        }
+    };
+    
+    ctrl.isValid = function () {
+        if(ctrl.password && ctrl.repeatedPassword){
+            return ctrl.name && ctrl.email && ctrl.password.length > 0 && ctrl.repeatedPassword.length > 0;
+        }else{
+            return false;
+        }
+    } ;
+    
+    ctrl.goToLanding = function () {
+        $uibModalInstance.close();
+        $location.path('/');
+    };
+});
