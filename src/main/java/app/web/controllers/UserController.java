f1239759c2fba0bfed04253f39e90041c7b29e89
Manu SOdhi <mssodhi@ucdavis.edu>
2016-06-05 21:02:08 -0700

    Just adding comments

--- src/main/java/app/web/controllers/UserController.java
+++ src/main/java/app/web/controllers/UserController.java
@@ -46,11 +46,14 @@ public class UserController {
     @RequestMapping(value = "addUser", method = RequestMethod.PUT)
     public String addUser (@RequestBody TempUser tempUser) {
 
+        // first create a temp user. Once they click the verify link,
+        // then you add them to the actual user table
         tempUser = tempUserService.save(tempUser);
         String[] recipients = {tempUser.getEmail()};
         String url = "http://localhost:8080/soundbox/#/verify/" + tempUser.getSecret();
         String messageBody = "<a href="+ url +">Verify Account</a>";
-        emailHelper.sendFromGMail(recipients, "SoundBox Account Verification", messageBody);
+        String subject = "SoundBox Account Verification for " + tempUser.getName();
+        emailHelper.sendFromGMail(recipients, subject, messageBody);
 
         return "{\"code\":\"1\"}";
 
@@ -58,9 +61,14 @@ public class UserController {
 
     @RequestMapping(value = "verify/{secret}", method = RequestMethod.GET)
     public String verifyAccount(@PathVariable String secret){
+        // verify temp user and make sure he exists.
         TempUser tempUser = tempUserService.getUserByCode(secret);
         if(tempUser != null){
+            // make sure this email isn't being used anywhere
+            // checkAvailability should've checked it before, but this
+            // is just an extra check to be sure
             if(userService.findByEmail(tempUser.getEmail()) == null){
+                // create the user from temp user, save, and then delete temp user
                 User user = new User();
                 user.setEmail(tempUser.getEmail());
                 user.setName(tempUser.getName());
@@ -70,6 +78,8 @@ public class UserController {
                 tempUserService.deleteTempUser(tempUser.getEmail());
                 return userService.toSimpleJson(user);
             }else{
+                // this should never happen. checkAvailability should've
+                // checked it already
                 System.out.println("User already exists. Verify method." + tempUser.getEmail());
                 return null;
             }
