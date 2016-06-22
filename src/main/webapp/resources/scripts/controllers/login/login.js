f8d36692f1bcdd096d9dd7447fce8fd91808dbb8
Manu SOdhi <mssodhi@ucdavis.edu>
2016-06-22 01:06:53 -0700

    added functionality to put a carousel with html templates

--- src/main/webapp/resources/scripts/controllers/login/login.js
+++ src/main/webapp/resources/scripts/controllers/login/login.js
@@ -2,11 +2,37 @@
 
 angular.module('app').controller('LoginCtrl', function ($location, profile, $uibModal) {
     var ctrl = this;
-    
+
+    var slides = ctrl.slides = [];
+    var currIndex = 0;
+
     ctrl.init = function () {
+        ctrl.myInterval = 3000;
+        ctrl.active = 0;
+
         if(profile.id != null){
             $location.path('/');
         }
+
+        for(var i = 0; i < 2; i++){
+            ctrl.addSlides();
+        }
+
+    };
+
+    ctrl.addSlides = function() {
+        var url;
+
+        if(currIndex === 0){
+            url = 'resources/scripts/controllers/login/carousel/first.html'
+        }
+        if(currIndex === 1){
+            url = 'resources/scripts/controllers/login/carousel/second.html'
+        }
+        slides.push({
+            url: url,
+            id: currIndex++
+        });
     };
 
     ctrl.openModal = function () {
