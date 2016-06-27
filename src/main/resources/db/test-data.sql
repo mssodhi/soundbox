5ba8a4a1f51a60f53fb26d8a3d7a6566a509f724
Manu SOdhi <mssodhi@ucdavis.edu>
2016-06-27 07:52:26 -0700

    test data and styling fix

--- src/main/resources/db/test-data.sql
+++ src/main/resources/db/test-data.sql
@@ -3,10 +3,12 @@ INSERT INTO soundbox.favorites ( `user_email`, `artist_id`, `user_id`) VALUES ('
 INSERT INTO soundbox.favorites ( `user_email`, `artist_id`, `user_id`) VALUES ('root', '36960179', 1 )
 INSERT INTO soundbox.favorites ( `user_email`, `artist_id`, `user_id`) VALUES ('root', '44513723', 1 )
 INSERT INTO soundbox.favorites ( `user_email`, `artist_id`, `user_id`) VALUES ('root', '8145658', 1 )
+INSERT INTO soundbox.favorites ( `user_email`, `artist_id`, `user_id`) VALUES ('root', '117052852', 1 )
+INSERT INTO soundbox.favorites ( `user_email`, `artist_id`, `user_id`) VALUES ('root', '41792998', 1 )
 
 
-INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('1', 'Workout', 0, 1 )
-INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('2', 'Jazz', 0, 1 )
+INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('1', 'Summer 2K16', 0, 1 )
+INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('2', 'Canyon Lake 2K16', 0, 1 )
 
 INSERT INTO soundbox.playlistsong ( `id`, `track_id`, `playlist_id`) VALUES (1, 171440547, 1 )
 INSERT INTO soundbox.playlistsong ( `id`, `track_id`, `playlist_id`) VALUES (2, 239663611, 1 )
