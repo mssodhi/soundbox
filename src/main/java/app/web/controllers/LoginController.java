2675d78c723275be7234ef1353273c912c35ca01
Manu SOdhi <mssodhi@ucdavis.edu>
2016-06-30 00:24:24 -0700

    Added velocity engine to send an e-mail template

--- src/main/java/app/web/controllers/LoginController.java
+++ src/main/java/app/web/controllers/LoginController.java
@@ -5,8 +5,15 @@ import app.web.domain.User;
 import app.web.helper.EmailHelper;
 import app.web.services.TempUserService;
 import app.web.services.UserService;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.StringWriter;
 
 @RestController
 @RequestMapping(value = "/api/login/")
@@ -20,6 +27,8 @@ public class LoginController {
 
     @Autowired
     EmailHelper emailHelper;
+
+    private VelocityEngine velocityEngine;
 
     @RequestMapping(value = "checkAvailability/{email:.+}", method = RequestMethod.GET)
     public String checkAvailability (@PathVariable String email) {
@@ -35,16 +44,30 @@ public class LoginController {
     }
 
     @RequestMapping(value = "addUser", method = RequestMethod.PUT)
-    public String addUser (@RequestBody TempUser tempUser) {
-
+    public String addUser (@RequestBody TempUser tempUser) throws Exception {
         // first create a temp user. Once they click the verify link,
         // then you add them to the actual user table
         tempUser = tempUserService.save(tempUser);
         String[] recipients = {tempUser.getEmail()};
+
         String url = "http://localhost:8080/soundbox/#/verify/" + tempUser.getSecret();
-        String messageBody = "<a href="+ url +">Verify Account</a>";
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
+        /*  next, get the Template  */
+        Template t = ve.getTemplate("email-templates/verify-email.vm");
+
+        VelocityContext context = new VelocityContext();
+        context.put("user", tempUser);
+        context.put("link", url);
+
+        /* now render the template into a StringWriter */
+        StringWriter messageBody = new StringWriter();
+        t.merge( context, messageBody );
+
         String subject = "SoundBox Account Verification for " + tempUser.getName();
-        emailHelper.sendFromGMail(recipients, subject, messageBody);
+        emailHelper.sendFromGMail(recipients, subject, messageBody.toString());
 
         return "{\"code\":\"1\"}";
 
