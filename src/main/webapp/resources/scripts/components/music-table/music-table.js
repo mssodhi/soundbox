0c148ca0ca3087fc2d44b1d552e9d15c7e6f8fec
Manu SOdhi <mssodhi@ucdavis.edu>
2016-07-11 02:57:20 -0700

    added pagination to the music table

--- src/main/webapp/resources/scripts/components/music-table/music-table.js
+++ src/main/webapp/resources/scripts/components/music-table/music-table.js
@@ -5,7 +5,10 @@ angular.module('app').component("musicTable", {
     controllerAs: 'ctrl',
     controller: function (MusicService, PlaylistService) {
         var ctrl = this;
-
+        ctrl.currentPage = 1;
+        ctrl.itemsPerPage = 20;
+        // ctrl.pages = ctrl.tracks.length / 20;
+        
         var sb_date, sb_title, sb_duration, sb_artist = false;
         var sb_plays = true;
 
