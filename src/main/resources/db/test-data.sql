7ce30af915b2d3474facc6dcaa4b97f3ade98d2c
Manu SOdhi <mssodhi@ucdavis.edu>
2016-07-19 12:03:04 -0700

    adding test data

--- src/main/resources/db/test-data.sql
+++ src/main/resources/db/test-data.sql
@@ -1,3 +1,23 @@
+INSERT INTO soundbox.user (`id`, `name`, `email`,`created_date`, `last_modified_date`, `fb_id`) VALUES ('1', 'Manu Sodhi', 'mssodhi@ucdavis.edu', '2016-06-05 12:47:00', '2016-06-05 12:47:00', '10205828170845240')
+
+INSERT INTO soundbox.favorites (`artist_id`, `user_id`) VALUES ('22409434', 1 )
+INSERT INTO soundbox.favorites (`artist_id`, `user_id`) VALUES ('36960179', 1 )
+INSERT INTO soundbox.favorites (`artist_id`, `user_id`) VALUES ('44513723', 1 )
+INSERT INTO soundbox.favorites (`artist_id`, `user_id`) VALUES ('8145658', 1 )
+INSERT INTO soundbox.favorites (`artist_id`, `user_id`) VALUES ('117052852', 1 )
+INSERT INTO soundbox.favorites (`artist_id`, `user_id`) VALUES ('41792998', 1 )
+
+
+INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('1', 'Summer 2K16', 0, 1 )
+INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('2', 'Canyon Lake 2K16', 0, 1 )
+INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('3', 'Workout', 0, 1 )
+INSERT INTO soundbox.playlist ( `id`, `name`, `is_private`, `user_id`) VALUES ('4', 'MUUSiC', 0, 1 )
+
+INSERT INTO soundbox.playlistsong ( `id`, `track_id`, `playlist_id`) VALUES (1, 171440547, 1 )
+INSERT INTO soundbox.playlistsong ( `id`, `track_id`, `playlist_id`) VALUES (2, 239663611, 1 )
+INSERT INTO soundbox.playlistsong ( `id`, `track_id`, `playlist_id`) VALUES (5, 239663611, 2 )
+INSERT INTO soundbox.playlistsong ( `id`, `track_id`, `playlist_id`) VALUES (6, 257498312, 2 )
+
 INSERT INTO soundbox.genres (`id`, `name`) VALUES ('1', 'Alternative Rock')
 INSERT INTO soundbox.genres (`id`, `name`) VALUES ('2', 'Ambient')
 INSERT INTO soundbox.genres (`id`, `name`) VALUES ('3', 'Classical')
