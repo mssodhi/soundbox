27fe3ee8843d144171ae94fa34e4e472dd7deba8
Manu SOdhi <mssodhi@ucdavis.edu>
2016-07-23 13:30:51 -0700

    added ui for charts

--- src/main/webapp/resources/scripts/controllers/charts/charts.js
+++ src/main/webapp/resources/scripts/controllers/charts/charts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app').controller('ChartsCtrl', function (ChartsService) {
+angular.module('app').controller('ChartsCtrl', function (ChartsService, MusicService) {
     var ctrl = this;
     ctrl.tracks = [];
 
@@ -11,11 +11,33 @@ angular.module('app').controller('ChartsCtrl', function (ChartsService) {
 
     function getPopular() {
         ChartsService.get().$promise.then(function (res) {
-            console.log(res);
-            res.collection.forEach(function (track) {
-                ctrl.tracks.push(track);
+            res.collection.forEach(function (song) {
+                ctrl.tracks.push(song.track);
             })
         });
     }
+
+    ctrl.select = function (track) {
+        if(!ctrl.isPlaying(track)){
+            SC.stream('/tracks/' + track.id, {autoPlay: false}).then(function (player) {
+                player.seek(0);
+                player.on('finish', function () {
+                    player.seek(0);
+                    ctrl.select(MusicService.getNext());
+                });
+                MusicService.setPlayer(player, track);
+                MusicService.setList(ctrl.tracks);
+            });
+        }
+    };
+
+    ctrl.isPlaying = function (track) {
+        var playing = MusicService.getTrack();
+        if (playing) {
+            if (playing.id === track.id) {
+                return true;
+            }
+        }
+    };
 
 });
